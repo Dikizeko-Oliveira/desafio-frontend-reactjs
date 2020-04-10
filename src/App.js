0c905ee8b6088a2eb518785b90214a5b0671c4e2
Dikizeko-Oliveira <dikezekodagraca04@gmail.com>
2020-04-09 21:25:23 -0300

    desafio terminado

--- src/App.js
+++ src/App.js
@@ -1,26 +1,46 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import "./styles.css";
 
+import api from "./services/api";
+
 function App() {
+  const [repositories, setRepositories] = useState([]);
+
+  useEffect(() => {
+    api.get("repositories").then((response) => {
+      setRepositories(response.data);
+    });
+  }, []);
+
   async function handleAddRepository() {
-    // TODO
+    const response = await api.post("repositories", {
+      title: `Novo repositório ${Date.now()}`,
+      url: "https://github.com/Dikizeko-Oliveira/desafio-frontend-reactjs",
+      techs: ["React", "Node.js"],
+    });
+    //adiciona o novo repositório no final, mantendo os repositórios que já estavam no array
+    setRepositories([...repositories, response.data]);
   }
 
   async function handleRemoveRepository(id) {
-    // TODO
+    await api.delete(`repositories/${id}`);
+
+    //retorna todos os repositórios com id diferente do id recebido como parámetro.
+    setRepositories(repositories.filter((repository) => repository.id !== id));
   }
 
   return (
     <div>
       <ul data-testid="repository-list">
-        <li>
-          Repositório 1
-
-          <button onClick={() => handleRemoveRepository(1)}>
-            Remover
-          </button>
-        </li>
+        {repositories.map((repository) => (
+          <li key={repository.id}>
+            {repository.title}
+            <button onClick={() => handleRemoveRepository(repository.id)}>
+              Remover
+            </button>
+          </li>
+        ))}
       </ul>
 
       <button onClick={handleAddRepository}>Adicionar</button>
